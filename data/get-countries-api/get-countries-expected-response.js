3726725fa18ce9de505d27b4adeb5317ee50216c
nguyenthuydung1603 <nthuydung.1603@gmail.com>
2024-08-31 09:44:28 +0700

    [PJS-1237] Verify Get Countries API Return Data Correctly

---
+++ data/get-countries-api/get-countries-expected-response.js
@@ -0,0 +1,38 @@
+export const expectedCountries = [
+  {
+    name: "USA",
+    code: "US",
+  },
+  {
+    name: "Viet Nam",
+    code: "VN",
+  },
+  {
+    name: "Canada",
+    code: "CA",
+  },
+  {
+    name: "UK",
+    code: "GB",
+  },
+  {
+    name: "France",
+    code: "FR",
+  },
+  {
+    name: "Japan",
+    code: "JP",
+  },
+  {
+    name: "India",
+    code: "IN",
+  },
+  {
+    name: "China",
+    code: "CN",
+  },
+  {
+    name: "Brazil",
+    code: "BR",
+  },
+];
--- tests/get-countries-api/get-countries-api.spec.js
+++ tests/get-countries-api/get-countries-api.spec.js
@@ -2,6 +2,8 @@ const { test, expect } = require("@playwright/test");
 const Ajv = require("ajv");
 const ajv = new Ajv();
 const getCountriesJsonSchema = require("../../data/get-countries-api/get-countries-api-json-schema.json");
+const { request } = require("http");
+const {expectedCountries} = require("../../data/get-countries-api/get-countries-expected-response");
 
 test("Verify get countries api response schema", async ({ request }) => {
   const url = "http://localhost:3000/api/v1/countries";
@@ -14,4 +16,18 @@ test("Verify get countries api response schema", async ({ request }) => {
   console.log("API info: ", { url, actualResponseBody, expectedResponse });
   expect(validateResult).toBeTruthy();
   expect(response.status()).toBe(200);
+});
+
+test("Verify get countries api return data correctly", async ({ request }) => {
+  const url = "http://localhost:3000/api/v1/countries";
+  const response = await request.get(url);
+  const actualResponseBody = await response.json();
+  console.log("Api info: ", {
+    url,
+    actualResponseBody,
+    expectedCountries,
+  });
+  expect(response.status()).toBe(200);
+  expect(actualResponseBody).toEqual(expect.arrayContaining(expectedCountries));
+  expect(expectedCountries).toEqual(expect.arrayContaining(actualResponseBody));
 });
